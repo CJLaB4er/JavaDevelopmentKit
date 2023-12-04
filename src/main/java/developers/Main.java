058bdd02e9b5d5a2775f96e24e81cd85c253fa3d
CJlaB4er-MSI <CJlaB4er@gmail.com>
2023-12-05 01:06:26 +0300

    Выполнено ДЗ второго семинара.

--- src/main/java/developers/Main.java
+++ src/main/java/developers/Main.java
@@ -2,13 +2,16 @@ package developers;
 
 public class Main {
     public static void main(String[] args) {
-        Backender backender = new Backender();
-        Frontender frontender = new Frontender();
-        FullStackMan fullStackMan = new FullStackMan();
+        Developer backender = new Backender();
+        if (backender instanceof Backender) ((Backender) backender).serverCoding();
 
-        backender.serverCoding();
-        frontender.frontendCoding();
-        fullStackMan.serverCoding();
-        fullStackMan.frontendCoding();
+        Developer frontender = new Frontender();
+        if (frontender instanceof Frontender) ((Frontender) frontender).frontendCoding();
+
+        Developer fullStackMan = new FullStackMan();
+        if (fullStackMan instanceof FullStackMan) {
+            ((FullStackMan) fullStackMan).serverCoding();
+            ((FullStackMan) fullStackMan).frontendCoding();
+        }
     }
 }
